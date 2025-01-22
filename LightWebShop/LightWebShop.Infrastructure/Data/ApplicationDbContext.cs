53baa146cb3982fee444b54d78845957b23656ed
dxn7i <d.ivanov2020@pmg-pernik.com>
2025-01-22 16:12:13 +0200

    ProjectWithDb

---
+++ LightWebShop/LightWebShop.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LightWebShop.Infrastructure.Data.Domain;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LightWebShop.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext <ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Brand> Brands { get; set; }  
+        public DbSet<Category> Categories { get; set; }  
+        public DbSet<Product> Products { get; set; }  
+        public DbSet<Order> Orders { get; set; }  
+    }
+}
